6f014c7b79e84dfb1563a3db4aa2fc4c16a225b7
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2023-10-02 12:22:21 +0200

    feature: header init

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,30 @@
 import { FC } from "react";
+import Link from "next/link";
+import { CartIcon, HamburgerIcon, Logo } from "@/ui";
 import styles from "./Header.module.scss";
 
 export const Header: FC = () => {
-  return <header className={styles.container}>header</header>;
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <HamburgerIcon className={styles.hamburger} />
+        <Logo />
+        <nav className={styles.menu}>
+          <Link className={styles.menuItem} href="/">
+            Home
+          </Link>
+          <Link className={styles.menuItem} href="/headphones">
+            Headphones
+          </Link>
+          <Link className={styles.menuItem} href="/speakers">
+            Speakers
+          </Link>
+          <Link className={styles.menuItem} href="/earphones">
+            Earphones
+          </Link>
+        </nav>
+        <CartIcon className={styles.cartIcon} />
+      </header>
+    </div>
+  );
 };
--- ui/svg/HamburgerIcon.tsx
+++ ui/svg/HamburgerIcon.tsx
@@ -1,6 +1,6 @@
-import { FC } from "react";
+import { ComponentProps, FC } from "react";
 
-export const HamburgerIcon: FC = () => {
+export const HamburgerIcon: FC<ComponentProps<"svg">> = (props) => {
   return (
     <svg
       width="16"
@@ -9,6 +9,7 @@ export const HamburgerIcon: FC = () => {
       fill="#FFF"
       fillRule="evenodd"
       viewBox="0 0 16 15"
+      {...props}
     >
       <path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z" />
     </svg>
