3ed5de09a867d5a16b4c34958611b80b3a8e8ccd
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2023-10-28 23:33:44 +0200

    feature: improved images and categories

--- components/Categories/Categories.tsx
+++ components/Categories/Categories.tsx
@@ -1,50 +1,45 @@
 import { FC } from "react";
-import styles from "./Categories.module.scss";
-import { ArrowRightIcon, Button, Typography } from "@/ui";
 import Image from "next/image";
+import { earphones, headphones, speakers } from "@/images";
+import { ArrowRightIcon, Button, Typography } from "@/ui";
+import styles from "./Categories.module.scss";
+
+const categories = [
+  {
+    image: headphones,
+    label: "Headphones",
+    href: "/headphones",
+  },
+  {
+    image: speakers,
+    label: "Speakers",
+    href: "/speakers",
+  },
+  {
+    image: earphones,
+    label: "Earphones",
+    href: "/earphones",
+  },
+];
 
 export const Categories: FC = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.box}>
-        <div className={styles.image}>
-          <Image fill src="/images/desktop/headphones.png" alt="earphones" />
-        </div>
-        <Typography variant="body" className={styles.label}>
-          Speakers
-        </Typography>
-        <div>
+      {categories.map((category) => (
+        <div className={styles.box} key={category.label}>
+          <Image
+            className={styles.image}
+            src={category.image}
+            alt={category.label}
+          />
+          <Typography variant="body" className={styles.label}>
+            {category.label}
+          </Typography>
           <Button variant="tertiary">
             Shop <ArrowRightIcon />
           </Button>
         </div>
-      </div>
-      <div className={styles.box}>
-        <div className={styles.image}>
-          <Image fill src="/images/desktop/speakers.png" alt="speakers" />
-        </div>
-        <Typography variant="body" className={styles.label}>
-          Speakers
-        </Typography>
-        <div>
-          <Button variant="tertiary">
-            Shop <ArrowRightIcon />
-          </Button>
-        </div>
-      </div>
-      <div className={styles.box}>
-        <div className={styles.image}>
-          <Image fill src="/images/desktop/earphones.png" alt="earphones" />
-        </div>
-        <Typography variant="body" className={styles.label}>
-          Earphones
-        </Typography>
-        <div>
-          <Button variant="tertiary">
-            Shop <ArrowRightIcon />
-          </Button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
---
+++ images/shared/index.ts
@@ -0,0 +1,5 @@
+import earphones from "./earphones.png";
+import headphones from "./headphones.png";
+import speakers from "./speakers.png";
+
+export { earphones, headphones, speakers };
