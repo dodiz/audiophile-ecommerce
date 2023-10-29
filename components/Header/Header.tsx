52d9d6a6eba7fad572176ed2db1692710dce45c5
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2023-10-29 22:36:40 +0100

    feature: cart icon hover

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,32 +9,34 @@ import styles from "./Header.module.scss";
 export const Header: FC<HeaderProps> = ({ variant = "absolute" }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   return (
-    <div className={classNames(styles.container, styles[variant])}>
-      <header className={styles.header}>
-        <HamburgerIcon className={styles.hamburger} />
-        <Link href="/">
-          <Logo />
-        </Link>
-        <nav className={styles.menu}>
-          <Link className={styles.menuItem} href="/">
-            Home
+    <>
+      <CartDialog show={isCartOpen} onHide={() => setIsCartOpen(false)} />
+      <div className={classNames(styles.container, styles[variant])}>
+        <header className={styles.header}>
+          <HamburgerIcon className={styles.hamburger} />
+          <Link href="/">
+            <Logo />
           </Link>
-          <Link className={styles.menuItem} href="/headphones">
-            Headphones
-          </Link>
-          <Link className={styles.menuItem} href="/speakers">
-            Speakers
-          </Link>
-          <Link className={styles.menuItem} href="/earphones">
-            Earphones
-          </Link>
-        </nav>
-        <CartIcon
-          className={styles.cartIcon}
-          onClick={() => setIsCartOpen((p) => !p)}
-        />
-        <CartDialog show={isCartOpen} onHide={() => setIsCartOpen(false)} />
-      </header>
-    </div>
+          <nav className={styles.menu}>
+            <Link className={styles.menuItem} href="/">
+              Home
+            </Link>
+            <Link className={styles.menuItem} href="/headphones">
+              Headphones
+            </Link>
+            <Link className={styles.menuItem} href="/speakers">
+              Speakers
+            </Link>
+            <Link className={styles.menuItem} href="/earphones">
+              Earphones
+            </Link>
+          </nav>
+          <CartIcon
+            className={styles.cartIcon}
+            onClick={() => setIsCartOpen((p) => !p)}
+          />
+        </header>
+      </div>
+    </>
   );
 };
